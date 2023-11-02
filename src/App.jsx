a4d06016b48662c0da05656c9115ee9c9451a9ca
rafiks <rab@devsphoto.com>
2023-11-01 22:45:23 -0400

    build routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import Home from "./Pages/Home"
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import About from "./Pages/About"
+import Profile from "./Pages/Profile"
+import SignIn from "./Pages/SignIn"
+import SignUp from "./Pages/SignUp"
 
 export default function App() {
   return (
-    <h1 className='text-red-500'>App</h1>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  ); 
 }
