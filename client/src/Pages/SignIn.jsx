3b36515179b673b569a5d6d17839aaf4ae7480c7
rafiks <rab@devsphoto.com>
2023-11-12 21:23:00 -0500

    add redux tk

--- client/src/Pages/SignIn.jsx
+++ client/src/Pages/SignIn.jsx
@@ -1,11 +1,15 @@
 import { React, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { signInStart, signInFailure, signInSuccess } from "../redux/user/userSlice";
 
 export default function SignIn() {
   const [formData, setFormData] = useState({}); //[username, email, password
-  const [error, setError] = useState("");
-  const [loading, setLoading] = useState(false);
+  //const [error, setError] = useState("");
+  //const [loading, setLoading] = useState(false);
+  const { error, loading } = useSelector((state) => state.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
@@ -14,6 +18,8 @@ export default function SignIn() {
     // add a try and catch block to catch error with signup
 
     try {
+      //setLoading(true);
+      dispatch(signInStart());
       const res = await fetch("/api/auth/signin", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
@@ -21,16 +27,19 @@ export default function SignIn() {
       }); // goto vite.config.js to add proxy
       const data = await res.json();
       if (data.success === false) {
-        setError(data.message);
-        setLoading(false);
+        //setError(data.message);
+        //setLoading(false);
+        dispatch(signInFailure(data.message));
         return;
       }
-      setLoading(false);
-      setError(null);
+      //setLoading(false);
+      //setError(null);
+      dispatch(signInSuccess(data));
       navigate("/");
     } catch (error) {
-      setLoading(false);
-      setError(error.message);
+      //setLoading(false);
+      //setError(error.message);
+      dispatch(signInFailure(error.message));
     }
   };
   return (
