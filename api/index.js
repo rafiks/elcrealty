62a1bff259a53a32a5f9a3544c164e028dd14ced
rafiks <rab@devsphoto.com>
2023-12-02 20:47:25 -0500

    add create listing api route

--- api/index.js
+++ api/index.js
@@ -1,9 +1,10 @@
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
-import dotenv from "dotenv";
-import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
-import cookieParser from "cookie-parser";
+import userRouter from "./routes/user.route.js";
+import listingRouter from "./routes/listing.route.js";
 
 dotenv.config();
 mongoose
@@ -26,6 +27,7 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 
 // middleware is where we can handle errors
 app.use((err, req, res, next) => {
