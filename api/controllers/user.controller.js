bf8609d168b88a1540c7f502230a97b823f91e67
rafiks <rab@devsphoto.com>
2023-12-24 17:18:55 -0500

    create get user listings api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import bcryptjs from "bcryptjs";
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
+import Listing from "../models/listing.model.js";
 
 export const test = (req, res) => {
   res.json({ message: "Api route works!" });
@@ -17,14 +18,14 @@ export const updateUser = async (req, res, next) => {
     const updatedUser = await User.findByIdAndUpdate(
       req.params.id,
       {
-      $set: {
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password,
-        avatar: req.body.avatar,
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          avatar: req.body.avatar,
+        },
       },
-    },
-    { new: true }
+      { new: true }
     );
     const { password, ...rest } = updatedUser._doc;
     res.status(200).json(rest);
@@ -35,14 +36,30 @@ export const updateUser = async (req, res, next) => {
 
 export const deleteUser = async (req, res, next) => {
   if (req.user.id !== req.params.id) {
+return next(errorHandler(401, "You can only delete your own profile!"));
+    try {
+      await User.findByIdAndDelete(req.params.id);
+      res.clearCookie("access_token");
+      res.status(200).json({ message: "User has been deleted..." });
+    }
+    catch (error) {
+      next(error);
+    }
+  } else {
+
     return next(errorHandler(401, "You can only delete your own profile!"));
   }
-  try {
-    await User.findByIdAndDelete(req.params.id);
-    res.clearCookie("access_token");
-    res.status(200).json("User deleted successfully!");
+};
+
+export const getUserListings = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
+    } catch (error) {
+      next(error);
     }
-   catch (error) {
-    next(error);
+  } else {
+    return next(errorHandler(401, "You can only view your own listings!"));
   }
 };
