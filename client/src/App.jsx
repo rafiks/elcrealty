8ac0e2777401d65e0fdf6b920dffdec769d179ab
rafiks <rab@devsphoto.com>
2024-01-19 23:14:21 -0500

    Create search page ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./Pages/CreateListing";
 import UpdateListing from "./Pages/UpdateListing";
 import Listing from "./Pages/Listing";
+import Search from "./Pages/Search";
 
 export default function App() {
   return (
@@ -28,6 +29,7 @@ export default function App() {
         </Route>
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/search" element={<Search />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ client/src/Pages/Search.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+
+export default function Search() {
+  return (
+    <div className="flex flex-col md:flex-row">
+      <div className="p-7 border-b-2 md:border-r-2 md:min-h-screen">
+        <form className="flex flex-col gap-8">
+          <div className="flex items-center gap-2">
+            <label className="whitespace-nowrap font-semibold">
+              Search Term
+            </label>
+            <input
+              type="text"
+              id="searchTerm"
+              placeholder="Search..."
+              className="rounded-lg border p-3 w-full"
+            />
+          </div>
+          <div className="flex gap-2 flex-wrap items-center">
+            <label className="font-semibold" htmlFor="">
+              Type:
+            </label>
+            <div className="flex gap-2">
+              <input type="checkbox" id="all" className="w-5" />
+              <span>Rent & Sale</span>
+            </div>
+            <div className="flex gap-2">
+              <input type="checkbox" id="rent" className="w-5" />
+              <span>Rent</span>
+            </div>
+            <div className="flex gap-2">
+              <input type="checkbox" id="sell" className="w-5" />
+              <span>Sell</span>
+            </div>
+            <div className="flex gap-2">
+              <input type="checkbox" id="offer" className="w-5" />
+              <span>Offer</span>
+            </div>
+          </div>
+          <div className="flex gap-2 flex-wrap items-center">
+            <label htmlFor="" className="font-semibold">
+              Ameneties:
+            </label>
+            <div className="flex gap-2">
+              <input type="checkbox" id="parking" className="w-5" />
+              <span>Parking</span>
+            </div>
+            <div className="flex gap-2">
+              <input type="checkbox" id="furnished" className="w-5" />
+              <span>Furnished</span>
+            </div>
+          </div>
+          <div className="flex items-center gap-2">
+            <label className="font-semibold">Sort:</label>
+            <select className="border rounded-lg p-2" id="sort_order" name="">
+              <option value="">Price high to low</option>
+              <option value="">Price low to high</option>
+              <option value="">Latest</option>
+              <option value="">Oldest</option>
+            </select>
+          </div>
+          <button className="rounded-lg border bg-slate-700 text-white font-semibold p-3 uppercase hover:opacity-75">
+            Search
+          </button>
+        </form>
+      </div>
+      <div className="font-semibold text-2xl mt-2">
+        <h1>Listing Results:</h1>
+      </div>
+    </div>
+  );
+}
